352d3641c232da2b9d67272dee845c68522a660e
TrigonometricDev <88750056+TrigonometricDev@users.noreply.github.com>
2022-01-15 16:38:45 -0800

    changelog ex

--- src/main/kotlin/com/tristian/changelog/gui/Changelog.kt
+++ src/main/kotlin/com/tristian/changelog/gui/Changelog.kt
@@ -25,14 +25,12 @@ class Changelog {
         const val CHANGELOG_ENTRY_TEXT_COLOR: Int = 0x00000
 
         /**
-         *
          * Change me. you can figure it out yourself.. You can find an example file in the repo tho..
-         *
          */
         val CHANGELOG_FILE_LOCATION = URL("https://github.io/jsonfile/whatever")
 
         /**
-         * If you want to use
+         * If you want to use the whole repository this works too...
          */
         var GIT_COMMIT_API_LINK = URL("https://api.github.com/repos/chrislgarry/Apollo-11/commits")
 
@@ -40,11 +38,13 @@ class Changelog {
         /**
          * The filetypes that this changelog will support reading.
          */
-
         val SUPPORTED_FILE_EXTENSIONS = listOf("yml", "json")
     }
 
     fun render() {
-        TODO("Not yet implemented")
+    }
+
+
+    init {
     }
 }
